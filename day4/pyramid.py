ebb63b2542f427371e178205a89e1a64ae753cb6
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2020-09-11 16:03:23 -0400

    fixed loop in pyramid

--- day4/pyramid.py
+++ day4/pyramid.py
@@ -3,8 +3,8 @@ def pyramid(n,char):
     ##need something to monitor spacing 
     ## 2n + 1 gives you number of starts
     ## n is number of rows we want which refers to left side spacing
-    for i in range(0, 9):
+    for i in range(0, n):
         formula = 2*(i) + 1
         print(" " * (n-i) + (formula * char))
 
-pyramid(9,"*")
+pyramid(20,"*")
